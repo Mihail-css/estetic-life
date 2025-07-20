7bfbadea3ba5e55c7270e6e75481a608c42c1e3a
Mihail-css <scooter-1982@yandex.ru>
2025-07-20 13:16:50 +0300

    draft

---
+++ js/index.js
@@ -0,0 +1,53 @@
+
+
+// // Мобильная навигация
+
+const navBtn = document.querySelector('.mobile-nav-btn');
+const nav = document.querySelector('.mobile-nav');
+const menuIcon = document.querySelector('.nav-icon');
+
+navBtn.onclick = function () {
+	nav.classList.toggle('mobile-nav--open');
+	menuIcon.classList.toggle('nav-icon--active');
+	// document.body.classList.toggle('no-scroll');
+};
+
+
+
+const navLinks = document.querySelectorAll(".mobile-nav a");
+console.log(navLinks);
+
+//обходим ссылки методом форич и убираем классы
+nav.addEventListener("click", function () {
+	//console.log ("click");
+	navBtn.classList.remove("nav-icon--active");//Убираем активный класс у иконки моб навигации
+	nav.classList.remove("mobile-nav--open");//Убираем активный класс у блока моб навигации
+})
+
+
+// swiper
+var swiper = new Swiper(".mySwiper", {
+
+	slidesPerView: "auto",
+	spaceBetween: 10,
+	loop: true,
+
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+	breakpoints: {
+		640: {
+			slidesPerView: 1,
+			spaceBetween: 20,
+		},
+		768: {
+			slidesPerView: 2,
+			spaceBetween: 40,
+		},
+		1024: {
+			slidesPerView: 3,
+			spaceBetween: 50,
+		},
+	},
+});
