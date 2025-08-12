c279d9e23f7c677c169e8b88f7e93c59a9689563
Mihail-css <scooter-1982@yandex.ru>
2025-08-12 20:56:52 +0300

    fix mobile nav

--- js/index.js
+++ js/index.js
@@ -1,5 +1,4 @@
 
-
 // // Мобильная навигация
 
 const navBtn = document.querySelector('.mobile-nav-btn');
@@ -7,21 +6,24 @@ const nav = document.querySelector('.mobile-nav');
 const menuIcon = document.querySelector('.nav-icon');
 
 navBtn.onclick = function () {
+	
 	nav.classList.toggle('mobile-nav--open');
 	menuIcon.classList.toggle('nav-icon--active');
-	// document.body.classList.toggle('no-scroll');
+	document.body.classList.toggle('no-scroll');
+	
 };
-
 
 const navLinks = document.querySelectorAll(".mobile-nav a");
 // console.log(navLinks);
 
 //обходим ссылки методом форич и убираем классы
 nav.addEventListener("click", function () {
-	//console.log ("click");
+	
 	navBtn.classList.remove("nav-icon--active");//Убираем активный класс у иконки моб навигации
 	nav.classList.remove("mobile-nav--open");//Убираем активный класс у блока моб навигации
+	
 	menuIcon.classList.remove('nav-icon--active');// делаем обратно в линии значек
+	document.body.classList.remove('no-scroll');// возващаем scrole
 })
 
 
@@ -36,6 +38,7 @@ var swiper = new Swiper(".mySwiper", {
 		el: ".swiper-pagination",
 		clickable: true,
 	},
+
 	breakpoints: {
 		640: {
 			slidesPerView: 1,
